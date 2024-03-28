f104855e62d3364c7b8cb6ba924e190a237fe010
Herve maurin <nzali2@hotmail.com>
2024-03-28 22:02:00 +0100

    variable

---
+++ variable.py
@@ -0,0 +1,18 @@
+import main
+
+main.print_hi("Johannes!")
+
+nummer = 10
+schuler = 12
+
+print(dir(nummer))
+print("Anzahl Schuler = " + str(schuler))
+print(type(nummer))
+schuler = "Ali"
+print(len(schuler))
+boolean = schuler == "Ali"
+print(boolean)
+print(schuler == "John")
+
+nummmer = 1.3
+print(type(nummmer))
